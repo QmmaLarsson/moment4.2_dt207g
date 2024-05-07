1a2686c9b739ba1b465679a4c07f7f1c95a36b58
Emma Larsson <emma940112@gmail.com>
2024-05-07 12:44:03 +0200

    Lagt till funktionalitet för inloggning

--- src/js/user.js
+++ src/js/user.js
@@ -55,3 +55,62 @@ async function addUser(newUsername, newPassword) {
     const data = await response.json();
     console.log(data);
 }
+
+//Logga in redan registrerad användare
+//Variabler
+const loginFormEl = document.getElementById("loginForm");
+
+//Händelsehanterare
+loginFormEl.addEventListener("submit", handleLogin);
+
+//Funktion för att hantera formulär
+async function handleLogin(event) {
+    //Förhindra standardsubmit
+    event.preventDefault();
+
+    //Hämta värden från inputfälten
+    const usernameEl = document.getElementById("username").value;
+    const passwordEl = document.getElementById("password").value;
+
+    // Validera input
+    if (!usernameEl.trim() || !passwordEl.trim()) {
+        alert("Vänligen fyll i alla fält")
+        return;
+    }
+
+    await login(usernameEl, passwordEl);
+
+    //Återställ formuläret
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+}
+
+//Funktion för att lägga till information till APIet
+async function login(username, password) {
+    //Länk till APIet
+    const url = "https://moment41dt207g.onrender.com/api/login";
+
+    let user = {
+        username: username,
+        password: password,
+    }
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "content-type": "Application/json"
+        },
+        body: JSON.stringify(user)
+    });
+
+    const data = await response.json();
+
+    if( response.status === 200) {
+        const token = data.response.token;
+        console.log(token);
+        localStorage.setItem("token", token);
+        window.location.href = "index.html";
+    } else {
+        console.log("Fel e-postadress eller lösenord");
+    }
+}
